bbe49a8746fe5ba46a8ac4b4dacfb814b120d9eb
dmalex <dmalex619@outlook.com>
2018-12-17 11:27:01 +0300

    Добавьте файлы проекта.

---
+++ TestINotifyPropertyChanged/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestINotifyPropertyChanged
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string currentTime = "";
+        public string CurrentTime
+        {
+            get { return currentTime; }
+            set
+            {
+                if (value != currentTime)
+                {
+                    currentTime = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int randNumber = 0;
+        public int RandNumber
+        {
+            get { return randNumber; }
+            set
+            {
+                if (value != randNumber)
+                {
+                    randNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ TestINotifyPropertyChanged/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestINotifyPropertyChanged
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel model = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = model;
+        }
+
+        private void btnTime_Click(object sender, RoutedEventArgs e)
+        {
+            model.CurrentTime = DateTime.Now.ToLongTimeString();
+        }
+
+        private void btnNumber_Click(object sender, RoutedEventArgs e)
+        {
+            model.RandNumber = new Random().Next();
+        }
+
+        public async void GetSomething()
+        {
+            // Кортежи определены в версии .Net 4.7
+            (int x, int y) tpl = (12, 45);
+            Console.WriteLine($"{tpl.x}   {tpl.y}");
+            Console.WriteLine("Test");
+
+            // Async/await определены в версии .Net 4.5
+            await Task.Run(() => Thread.Sleep(1000) );
+        }
+    }
+}
